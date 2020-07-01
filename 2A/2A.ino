e7262f1078dbd28bcf4c1c8f1fe8f7e434770b14
Fabo xw <55964635+FabianMartinez1234567@users.noreply.github.com>
2020-07-01 00:52:09 -0300

    prolijidad

--- 2A/2A.ino
+++ 2A/2A.ino
@@ -1,44 +1,58 @@
 bool estado=false;
 bool estadoAnt=false;
-void setup() {
+void setup()
+{
   pinMode(6, OUTPUT);
   pinMode(5, OUTPUT);    
   pinMode(4, OUTPUT);
   pinMode(3, OUTPUT);
   pinMode(2,INPUT);
 }
-void loop() {
-adelante();
-adelante();
-atras();
- }
-  void adelante(){
-  for(int i=3;i<=6;i++){
-      estado=digitalRead(2);
-    if(estado!=estadoAnt){
-    todos();
-    estadoAnt = estado;}
-   digitalWrite(i, HIGH);
-   delay(500);
-   digitalWrite(i,LOW);
-  }
-  }
- void atras(){
-    for(int i=6;i>=3;i--){
-      estado=digitalRead(2);
-    if(estado!=estadoAnt){
-    todos();
-    estadoAnt = estado;}
-   digitalWrite(i, HIGH);
-   delay(500);
-   digitalWrite(i,LOW);
-  }
+void loop()
+{
+    adelante();
+    adelante();
+    atras();
+}
+
+void adelante()
+{
+    for(int i=3;i<=6;i++)
+    {
+        estado=digitalRead(2);
+        if(estado!=estadoAnt)
+        {
+            todos();
+            estadoAnt = estado;
+        }
+    digitalWrite(i, HIGH);
+    delay(500);
+    digitalWrite(i,LOW);
+    }
+}
+
+
+void atras()
+{
+    for(int i=6;i>=3;i--)
+    {
+        estado=digitalRead(2);
+        if(estado!=estadoAnt)
+        {
+            todos();
+            estadoAnt = estado;
+        }
+        digitalWrite(i, HIGH);
+        delay(500);
+        digitalWrite(i,LOW);
+    }
   
-  
-  }
-void todos(){
-  digitalWrite(3, HIGH);
-  digitalWrite(4, HIGH);                              
-  digitalWrite(5, HIGH);
-  digitalWrite(6, HIGH);                              
- }
+}
+
+void todos()
+{
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);                              
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);                              
+}
