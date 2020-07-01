4e61e7c38e5edeaf3378c626d3e3e52b8fe71f10
Fabo xw <55964635+FabianMartinez1234567@users.noreply.github.com>
2020-07-01 01:02:51 -0300

    prolijidad

--- 2D/2D.ino
+++ 2D/2D.ino
@@ -1,32 +1,48 @@
 bool estado = false;
-void setup() {
-  pinMode(6, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(2, INPUT);
-  attachInterrupt(digitalPinToInterrupt(2), ALTO, CHANGE);
+
+void setup()
+{
+    pinMode(6, OUTPUT);
+    pinMode(5, OUTPUT);
+    pinMode(4, OUTPUT);
+    pinMode(3, OUTPUT);
+    pinMode(2, INPUT);
+    attachInterrupt(digitalPinToInterrupt(2), ALTO, CHANGE);
 }
-void loop() {
-if(estado==true){
-adelante();
-}else{
-atras();}
+
+void loop()
+{
+    if(estado==true)
+    {
+        adelante();
+    }
+    else
+    {
+        atras();
+    }
 }
-void adelante() {
-  for (int i = 3; i <= 6; i++) {
-    digitalWrite(i, HIGH);
-    delay(100);
-    digitalWrite(i, LOW);
+//__________________________________________________________________________________________________________________________________________________________________________________________
+void adelante()
+{
+    for (int i = 3; i <= 6; i++) 
+    {
+        digitalWrite(i, HIGH);
+        delay(100);
+        digitalWrite(i, LOW);
   }
 }
-void atras() {
-  for (int i = 6; i >= 3; i--) {
-    digitalWrite(i, HIGH);
-    delay(100);
-    digitalWrite(i, LOW);
-  }
+//__________________________________________________________________________________________________________________________________________________________________________________________
+void atras() 
+{
+    for (int i = 6; i >= 3; i--) 
+    {
+        digitalWrite(i, HIGH);
+        delay(100);
+        digitalWrite(i, LOW);
+    }
 }
-void ALTO() {
-estado=!estado;
+//__________________________________________________________________________________________________________________________________________________________________________________________
+void ALTO() 
+{
+    estado=!estado;
 }
